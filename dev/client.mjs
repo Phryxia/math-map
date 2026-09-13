79d3e4b53cd73433f4c469ac40eb072c3a7f132b
Phryxia <xahhaepica@gmail.com>
2026-09-13 18:21:38 +0900

    remove dev to restart cleanly

--- dev/client.mjs
+++
@@ -1,7 +0,0 @@
-import fs from 'node:fs'
-import {api, stop} from './run-api.mjs'
-const [service, route, filename] = process.argv.slice(2)
-try {
-  const body = filename ? JSON.parse(fs.readFileSync(filename, 'utf8')) : route === 'pop' ? {} : undefined
-  console.log(JSON.stringify(await api(service, route, body)))
-} finally { stop() }
